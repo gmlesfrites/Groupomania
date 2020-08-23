8aad67550af007f23538d7578eae1271f56fa39f
gmlesfrites <emilieviard.photographie@gmail.com>
2020-08-23 06:49:56 +0200

    retrouve erreur empeche connexion

--- frontend/src/store/auth.module.js
+++ frontend/src/store/auth.module.js
@@ -11,11 +11,6 @@ export const auth = {
   state: initialState,
   actions: {
     login({ commit }, user) {
-      getters: {
-        getAllUsers = () => {
-          return getters.state.user
-        }
-      }
       return AuthService.login(user).then(
         user => {
           commit('loginSuccess', user);
