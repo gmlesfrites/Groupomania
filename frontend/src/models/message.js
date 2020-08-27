3c4e28b2c40149d27b2eb5a83d4ef33760dbe5e2
gmlesfrites <emilieviard.photographie@gmail.com>
2020-08-27 07:09:51 +0200

    oubli parenthese

--- frontend/src/models/message.js
+++ frontend/src/models/message.js
@@ -1,6 +1,6 @@
 // modèle de données
 export default class Message {
-    constructor(title, content, id, userId, messageId, firstname, lastname) {
+    constructor(title, content, id, userId, messageId) {
       this.title = title;  //titre du message
       this.content = content;  //contenu
       this.id = id;  // id du message
