1c9d98ffc5f65023cea2c766cc2f06260429faa6
gmlesfrites <emilieviard.photographie@gmail.com>
2020-08-28 07:38:25 +0200

    design hello

--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -1,26 +1,29 @@
 <template>
   <v-container class="d-flex-col mt-12">
-    <h1>Bienvenue sur l'application Groupomania, votre réseau social d'entreprise</h1>
+    <h1>Bienvenue sur l'application Groupomania, <br/> votre réseau social d'entreprise</h1>
 
     <v-container class="d-flex-col mt-12">
       <v-row>
-        <p class="text-justify h4">Profitez dès à présent de cette application  dans un environnement sécurisé.</p>   
+        <p class="text-justify">Profitez dès à présent de cette application  dans un environnement sécurisé.</p>   
       </v-row>
       <v-row>
-        <p class="text-justify h4">Écrivez, partagez vos informations, des contenus d'actualité avec vos collègues de travail, que ce soit en lien avec votre métier, votre équipe ou même notre entreprise.</p>
+        <p class="text-justify">Écrivez, partagez vos informations, des contenus d'actualité avec vos collègues de travail, que ce soit en lien avec votre métier, votre équipe ou même notre entreprise.</p>
       </v-row>
 
       <v-row>
-        <p class="text-justify h4">Prenez garde à la facilité d'écrire ! </p>
-          
-        <p class="text-justify h4"> Quand on est "caché derrière son clavier", c'est plus facile de se 'lâcher' !</p>
-          
-        <p class="text-justify h4">Retrouvez toutes les conditions d'utilisation de cette application ici : 
+        <p class="text-justify">Prenez garde à la facilité d'écrire ! </p>
+      </v-row>
+
+      <v-row>
+        <p class="text-justify"> Quand on est "caché derrière son clavier", c'est plus facile de se "lâcher" !</p>
+      </v-row>
+
+      <v-row>
+        <p class="text-justify">Retrouvez toutes les conditions d'utilisation de cette application ici : 
           <br/>
           <router-link to="/terms"> Conditions d'utilisation </router-link> </p>
       </v-row>
     </v-container>
-
   </v-container>
 </template>
 
