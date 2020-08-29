2ab6ef37bb615c73810f1a4dc4343246731635e7
gmlesfrites <emilieviard.photographie@gmail.com>
2020-08-29 07:26:39 +0200

    id inutile bdd modifiee

--- frontend/src/models/user.js
+++ frontend/src/models/user.js
@@ -1,12 +1,11 @@
 // modèle de données
 export default class User {
-    constructor(firstname, lastname, bio, email, password, id, userId) {
+    constructor(firstname, lastname, bio, email, password, userId) {
       this.firstname = firstname; // prénom de l'utilisateur
       this.lastname = lastname;  // nom de l'utilisateur
       this.bio = bio; // petite bio sur le profil
       this.email = email;  // email
       this.password = password;   // mot de passe
-      this.id = id;
-      this.userId = userId;
+      this.userId = userId;       //identifiant
     }
   }
